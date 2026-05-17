3692ebb147980037b1e6342d9d20b7774a66a679
Lisa Buchanan <askenglishflow@gmail.com>
2026-05-17 17:30:44 +0800

    Refactor credit management logic in Dashboard and API; streamline profile fetching from USERS_TABLE and enhance daily credit deduction and reset processes

---
+++ android-twa/src/main/java/com/everydayenglishflow/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.everydayenglishflow.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
